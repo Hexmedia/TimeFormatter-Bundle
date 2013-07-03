5cd8d1614f1f7fe484327a3419262740ef7307ff
Krystian Kuczek <krystian@kuczek.eu>
2013-07-03 08:37:41 +0200

    Prepared for jenkins

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,12 @@
 
 namespace Hexmedia\TimeFormatterBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
 class Configuration implements ConfigurationInterface {
+
+	public function getConfigTreeBuilder() {
+
+	}
 
 }
