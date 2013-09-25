62393fcd7d383c4c533842ded3d9451b255af318
Krystian Kuczek <krystian@kuczek.eu>
2013-09-25 21:15:05 +0200

    Tests fixes, Polish translation fixes.

--- Templating/Helper/TimeFormatterHelper.php
+++ Templating/Helper/TimeFormatterHelper.php
@@ -13,172 +13,173 @@ use \Symfony\Component\Translation\TranslatorInterface;
 class TimeFormatterHelper extends Helper
 {
 
-	/**
-	 * Translator
-	 *
-	 * @var TranslatorInterface
-	 */
-	protected $translator;
+    /**
+     * Translator
+     *
+     * @var TranslatorInterface
+     */
+    protected $translator;
 
-	/**
-	 * Constructor
-	 *
-	 * @param TranslatorInterface $translator
-	 */
-	public function __construct(TranslatorInterface $translator)
-	{
-		$this->translator = $translator;
-	}
+    /**
+     * Constructor
+     *
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
 
-	/**
-	 * Get name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'time_formatter';
-	}
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'time_formatter';
+    }
 
-	/**
-	 * As $fromTime and $toTime we can use timestamp as int, @\DateTime or string with format
-	 * from $dateFormat
-	 *
-	 * @param \DateTime|string|int $fromTime
-	 * @param \DateTime|string|int $toTime
-	 * @param string               $format
-	 * @param string               $dateFormat
-	 *
-	 * @TODO: Format to be rewritten to use more specific input
-	 *
-	 * @return string
-	 */
-	public function formatTime($fromTime, $toTime = null, $format = null, $dateFormat = "Y-m-d H:i:s")
-	{
-		if ($toTime == null) {
-			$toTime = new \DateTime('now');
-		}
+    /**
+     * As $fromTime and $toTime we can use timestamp as int, @\DateTime or string with format
+     * from $dateFormat
+     *
+     * @param \DateTime|string|int $fromTime
+     * @param \DateTime|string|int $toTime
+     * @param string $format
+     * @param string $dateFormat
+     *
+     * @TODO: Format to be rewritten to use more specific input
+     *
+     * @return string
+     */
+    public function formatTime($fromTime, $toTime = null, $format = null, $dateFormat = "Y-m-d H:i:s")
+    {
+        if ($toTime == null) {
+            $toTime = new \DateTime('now');
+        }
 
-		$fromTime = $this->convertFormat($fromTime, $dateFormat);
-		$toTime = $this->convertFormat($toTime, $dateFormat);
+        $fromTime = $this->convertFormat($fromTime, $dateFormat);
+        $toTime = $this->convertFormat($toTime, $dateFormat);
 
-		$diff = $fromTime->diff($toTime);
+        $diff = $fromTime->diff($toTime);
 
-		if (strtolower($format) == "simple") {
-			return $this->formatSimple($diff);
-		} else {
-			return $this->formatNormal($diff);
-		}
-	}
+        if (strtolower($format) == "simple") {
+            return $this->formatSimple($diff);
+        } else {
+            return $this->formatNormal($diff);
+        }
+    }
 
-	/**
-	 * Formating using default format.
-	 *
-	 * @param \DateInterval $diff
-	 *
-	 * @return string
-	 */
-	private function formatNormal(\DateInterval $diff)
-	{
-		$str = '';
-		$first = null;
-		$second = null;
-		$val = 0;
-		if ($diff->y > 0) {
-			$str = "year";
-			$val = $diff->y;
-		} else if ($diff->m > 0) {
-			$str = "month";
-			$val = $diff->m;
-		} else if ($diff->d > 0) {
-			$str = "day";
-			$first = array('after' => "tomorrow", 'before' => "yesterday");
-			$val = $diff->d;
-		} else if ($diff->h > 0) {
-			$str = "hour";
-			$val = $diff->h;
-		} else if ($diff->i == 30) {
-			return $diff->invert ? $this->translator->trans("in next half hour") : $this->translator->trans("half hour ago");
-		} else if ($diff->i > 0) {
-			$str = "minute";
-			$val = $diff->i;
-		} else if ($diff->s > 0) {
-			$str = "second";
-			$val = $diff->s;
-		}
-		if ($diff->invert) {
-			$id = "%" . $str . "s%";
+    /**
+     * Formating using default format.
+     *
+     * @param \DateInterval $diff
+     *
+     * @return string
+     */
+    private function formatNormal(\DateInterval $diff)
+    {
+        $str = '';
+        $first = null;
+        $second = null;
+        $val = 0;
 
-			return $this->translator->transChoice(
-					($first && isset($first['after']) ? $first['after'] : "next " . $str) . "|" .
-					($second && isset($second['after']) ? $second['after'] : "in next " . $id . " " . $str . "s"), $val, array(
-					$id => $val
-			));
-		} else {
-			$id = "%" . $str . "%";
+        if ($diff->y > 0) {
+            $str = "year";
+            $val = $diff->y;
+        } else if ($diff->m > 0) {
+            $str = "month";
+            $val = $diff->m;
+        } else if ($diff->d > 0) {
+            $str = "day";
+            $first = array('after' => "tomorrow", 'before' => "yesterday");
+            $val = $diff->d;
+        } else if ($diff->h > 0) {
+            $str = "hour";
+            $val = $diff->h;
+        } else if ($diff->i == 30) {
+            var_dump($diff->invert);
+            return $diff->invert ? $this->translator->trans("next half hour") : $this->translator->trans("half hour ago");
+        } else if ($diff->i > 0) {
+            $str = "minute";
+            $val = $diff->i;
+        } else if ($diff->s > 0) {
+            $str = "second";
+            $val = $diff->s;
+        }
 
-			return $this->translator->transChoice(
-					($first && isset($first['before']) ? $first['before'] : ($str == "hour" ? 'an' : 'a') . ' ' . $str . ' ago') . "|" .
-					($second && isset($second['before']) ? $second['before'] : $id . " " . $str . "s ago"), $val, array(
-					$id => $val
-			));
-		}
-	}
+        $id = "%" . $str . "s%";
 
-	/**
-	 * Formating using simple format.
-	 *
-	 * @param \DateInterval $diff
-	 *
-	 * @return string
-	 */
-	private function formatSimple(\DateInterval $diff)
-	{
-		$str = '';
-		$pre = '';
-		if ($diff->y > 0 || $diff->m > 0 || $diff->d == 1 || $diff->h == 1) {
-			return $this->formatNormal($diff);
-		} else if ($diff->d > 0) {
-			$str = "few days";
-		} else if ($diff->h > 0) {
-			$str = "few hours";
-		} else if ($diff->i < 2) {
-			$str = "minute";
-			$pre = "a ";
-		} else if ($diff->i < 20) {
-			$str = "15 minutes";
-		} else if ($diff->i < 40) {
-			$str = "half hour";
-		} else if ($diff->i < 60) {
-			$str = "hour";
-			$pre = "an";
-		}
+        if ($diff->invert) {
+            return $this->translator->transChoice(
+                ($first && isset($first['after']) ? $first['after'] : "next " . $str) . "|" .
+                ($second && isset($second['after']) ? $second['after'] : "next " . $id . " " . $str . "s"), $val, array(
+                $id => $val
+            ));
+        } else {
+            return $this->translator->transChoice(
+                ($first && isset($first['before']) ? $first['before'] : ($str == "hour" ? 'an' : 'a') . ' ' . $str . ' ago') . "|" .
+                ($second && isset($second['before']) ? $second['before'] : $id . " " . $str . "s ago"), $val, array(
+                $id => $val
+            ));
+        }
+    }
 
-		return $this->translator->trans($diff->invert ? "in next " . $str : $pre . $str . " ago");
-	}
+    /**
+     * Formating using simple format.
+     *
+     * @param \DateInterval $diff
+     *
+     * @return string
+     */
+    private function formatSimple(\DateInterval $diff)
+    {
+        $str = '';
+        $pre = '';
+        if ($diff->y > 0 || $diff->m > 0 || $diff->d == 1 || $diff->h == 1) {
+            return $this->formatNormal($diff);
+        } else if ($diff->d > 0) {
+            $str = "few days";
+        } else if ($diff->h > 0) {
+            $str = "few hours";
+        } else if ($diff->i < 2) {
+            $str = "minute";
+            $pre = "a ";
+        } else if ($diff->i < 20) {
+            $str = "15 minutes";
+        } else if ($diff->i < 40) {
+            $str = "half hour";
+        } else if ($diff->i < 60) {
+            $str = "hour";
+            $pre = "an ";
+        }
 
-	/**
-	 * Converting date format to expected by methods
-	 *
-	 * @param \DateTime|string|int $time
-	 * @param string               $dateFormat
-	 *
-	 * @return \DateTime
-	 */
-	private function convertFormat($time, $dateFormat)
-	{
-		if (!($time instanceof \DateTime)) {
-			if (is_numeric($time)) {
-				$transformer = new DateTimeToTimestampTransformer();
-				$time = $transformer->reverseTransform($time);
-			} else {
-				$transformer = new DateTimeToStringTransformer(null, null, $dateFormat);
-				$time = $transformer->reverseTransport($time);
-			}
-		}
+        return $this->translator->trans($diff->invert ? "next " . $str : $pre . $str . " ago");
+    }
 
-		return $time;
-	}
+    /**
+     * Converting date format to expected by methods
+     *
+     * @param \DateTime|string|int $time
+     * @param string $dateFormat
+     *
+     * @return \DateTime
+     */
+    private function convertFormat($time, $dateFormat)
+    {
+        if (!($time instanceof \DateTime)) {
+            if (is_numeric($time)) {
+                $transformer = new DateTimeToTimestampTransformer();
+                $time = $transformer->reverseTransform($time);
+            } else {
+                $transformer = new DateTimeToStringTransformer(null, null, $dateFormat);
+                $time = $transformer->reverseTransport($time);
+            }
+        }
+
+        return $time;
+    }
 
 }
 
--- Tests/Templating/Helper/TimeFormatterHelperTest.php
+++ Tests/Templating/Helper/TimeFormatterHelperTest.php
@@ -3,6 +3,10 @@
 namespace Hexmedia\TimeFormatterBundle\Tests\Templating\Helper;
 
 use Hexmedia\TimeFormatterBundle\Templating\Helper\TimeFormatterHelper;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\Translator;
 
 /**
@@ -10,104 +14,275 @@ use Symfony\Component\Translation\Translator;
  */
 class TimeFormatterHelperTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
-     * @var TimeFormatterHelper
+     * @var array
      */
-    private $helper;
-
-    /**
-     * @var Translator
-     */
-    private $translator;
+    private $languages = array();
 
     /**
      * Test constructor
      */
     public function __construct()
     {
-        $this->translator = new Translator("en");
+        $languages = array('en');//, 'pl');
 
-        $this->helper = new TimeFormatterHelper($this->translator);
+        $yamlFileLoader = new YamlFileLoader();
+
+        $dir = dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'translations';
+
+
+        foreach ($languages as $lang) {
+            $translator = new Translator($lang);
+            $translator->addLoader("yml", $yamlFileLoader);
+
+            if ($lang != "en") {
+                $translator->addResource("yml", $dir . DIRECTORY_SEPARATOR . "messages.$lang.yml", "$lang", "messages", $lang);
+            }
+
+            $helper = new TimeFormatterHelper($translator);
+
+            $this->languages[$lang] = array(
+                'translator' => $translator,
+                'helper' => $helper
+            );
+        }
     }
 
     /**
-     * Testing simple ago format.
+     * Testing simple ago format year.
      */
-    public function testSimpleAgo()
+    public function testSimpleAgoYear()
     {
-        $this->makeTest('now -1 year', "a year ago", true);
-        $this->makeTest('now -1 month', "a month ago", true);
-        $this->makeTest('now -1 day', "yesterday", true);
-        $this->makeTest('now -2 day', "few days ago", true);
-        $this->makeTest('now -2 hour', "few hours ago", true);
-        $this->makeTest('now -1 hour', "an hour ago", true);
-        $this->makeTest('now -30 minutes', "half hour ago", true);
-        $this->makeTest('now -12 minutes', "15 minutes ago", true);
-        $this->makeTest('now -1 minute', "a minute ago", true);
-        $this->makeTest('now -1 second', "a minute ago", true);
+        $this->makeTest('year', true, "a year ago|%years% years ago", array(1 => 1, 2 => 2, 5 => 5, 10 => 10), true);
     }
 
     /**
-     * Testing simple next format.
+     * Testing simple ago format month.
      */
-    public function testSimpleNext()
+    public function testSimpleAgoMonth()
     {
-        $this->makeTest('now +1 year', "next year", true);
-        $this->makeTest('now +1 month', "next month", true);
-        $this->makeTest('now +1 day', "tomorrow", true);
-        $this->makeTest('now +1 hour', "next hour", true);
-        $this->makeTest('now +30 minutes', "in next half hour", true);
-        $this->makeTest('now +5 minute', "in next 15 minutes", true);
-        $this->makeTest('now +2 minute', "in next 15 minutes", true);
-        $this->makeTest('now +1 second', "in next minute", true);
+        $this->makeTest('month', true, 'a month ago|%months% months ago', array(1 => 1, 2 => 2, 5 => 5, 10 => 10), true);
     }
 
     /**
-     * Testing normal ago format.
+     * Testing simple ago format day.
      */
-    public function testNormalAgo()
+    public function testSimpleAgoDay()
     {
-        $this->makeTest('now -1 year', "a year ago");
-        $this->makeTest('now -1 month', "a month ago");
-        $this->makeTest('now -1 day', "yesterday");
-        $this->makeTest('now -1 hour', "an hour ago");
-        $this->makeTest('now -30 minutes', "half hour ago");
-        $this->makeTest('now -1 minute', "a minute ago");
-        $this->makeTest('now -1 second', "a second ago");
+        $this->makeTest('day', true, "yesterday|%days% days ago", array(1 => 1), true);
+        $this->makeSimpleTest("day", true, "few days ago", array(10, 15, 20), true);
     }
 
     /**
-     * Testing normal next format.
+     * Testing simple ago format hour.
      */
-    public function testNormalNext()
+    public function testSimpleAgoHours()
     {
-        $this->makeTest('now +1 year', "next year");
-        $this->makeTest('now +1 month', "next month");
-        $this->makeTest('now +1 day', "tomorrow");
-        $this->makeTest('now +1 hour', "next hour");
-        $this->makeTest('now +30 minutes', "in next half hour");
-        $this->makeTest('now +5 minute', "in next 5 minutes");
-        $this->makeTest('now +2 minute', "in next 2 minutes");
-        $this->makeTest('now +2 second', "in next 2 seconds");
-        $this->makeTest('now +1 second', "next second");
+        $this->makeTest('hour', true, 'an hour ago|%hours% hours ago', array(1 => 1), true);
+        $this->makeSimpleTest("hour", true, "few hours ago", array(10, 15, 20), true);
+
+    }
+
+    /**
+     * Testing simple ago format minute.
+     */
+    public function testSimpleAgoMinutes()
+    {
+        $this->makeTest('minute', true, "a minute ago|%minutes% minutes ago", array(1 => 1, 12 => 15, 10 => 15), true);
+        $this->makeSimpleTest("minute", true, "half hour ago", array(30, 34, 28), true);
+        $this->makeSimpleTest("minute", true, "an hour ago", array(46), true);
+    }
+
+    /**
+     * Testing normal ago format year.
+     */
+    public function testAgoYear()
+    {
+        $this->makeTest('year', true, "a year ago|%years% years ago", array(1 => 1, 2 => 2, 5 => 5, 10 => 10));
+    }
+
+    /**
+     * Testing normal ago format month.
+     */
+    public function testAgoMonth()
+    {
+        $this->makeTest('month', true, 'a month ago|%months% months ago', array(1 => 1, 2 => 2, 5 => 5, 10 => 10));
+    }
+
+    /**
+     * Testing normal ago format day.
+     */
+    public function testAgoDay()
+    {
+        $this->makeTest('day', true, "yesterday|%days% days ago", array(1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 10 => 10, 20 => 20));
+    }
+
+    /**
+     * Testing normal ago format hour.
+     */
+    public function testAgoHours()
+    {
+        $this->makeTest('hour', true, 'an hour ago|%hours% hours ago', array(1 => 1));
+
+    }
+
+    /**
+     * Testing normal ago format minute.
+     */
+    public function testAgoMinutes()
+    {
+        $this->makeTest('minute', true, "a minute ago|%minutes% minutes ago", array(1 => 1, 12 => 12, 10 => 10, 15 => 15));
+        $this->makeSimpleTest("minute", true, "half hour ago", array(30, 34, 28));
+    }
+
+    /**
+     * Testing normal ago format seconds.
+     */
+    public function testAgoSeconds()
+    {
+        $this->makeTest('second', true, 'a second ago|%seconds% seconds ago', array(1 => 1, 12 => 12, 10 => 10, 15 => 15));
+    }
+
+    /**
+     * Testing simple next format year.
+     */
+    public function testSimpleNextYear()
+    {
+        $this->makeTest('year', false, "next year|next %years% years", array(1 => 1, 2 => 2, 5 => 5, 10 => 10), true);
+    }
+
+    /**
+     * Testing simple next format month.
+     */
+    public function testSimpleNextMonth()
+    {
+        $this->makeTest('month', false, 'next month|next %months% months', array(1 => 1, 2 => 2, 5 => 5, 10 => 10), true);
+    }
+
+    /**
+     * Testing simple next format day.
+     */
+    public function testSimpleNextDay()
+    {
+        $this->makeTest('day', false, "tomorrow|next %days% days", array(1 => 1), true);
+        $this->makeSimpleTest("day", false, "next few days", array(10 => 10, 15 => 15, 20 => 20));
+    }
+
+    /**
+     * Testing simple next format hour.
+     */
+    public function testSimpleNextHours()
+    {
+        $this->makeTest('hour', false, 'next hour|next %hours% hours', array(1 => 1), true);
+        $this->makeSimpleTest("hour", false, "next few hours", array(10, 15, 20), true);
+
+    }
+
+    /**
+     * Testing simple next format minute.
+     */
+    public function testSimpleNextMinutes()
+    {
+        $this->makeTest('minute', false, "next minute|next %minutes% minutes", array(1 => 1, 12 => 15, 10 => 15), true);
+        $this->makeSimpleTest("minute", false, "next half hour", array(30, 34, 28), true);
+        $this->makeSimpleTest("minute", false, "next hour", array(46), true);
+    }
+
+    /**
+     * Testing normal next format year.
+     */
+    public function testNextYear()
+    {
+        $this->makeTest('year', false, "next year|next %years% years", array(1 => 1, 2 => 2, 5 => 5, 10 => 10));
+    }
+
+    /**
+     * Testing normal next format month.
+     */
+    public function testNextMonth()
+    {
+        $this->makeTest('month', false, 'next month|next %months% months', array(1 => 1, 2 => 2, 5 => 5, 10 => 10));
+    }
+
+    /**
+     * Testing normal next format day.
+     */
+    public function testNextDay()
+    {
+        $this->makeTest('day', false, 'tomorrow|next %days% days', array(1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 10 => 10, 20 => 20));
+    }
+
+    /**
+     * Testing normal next format hour.
+     */
+    public function testNextHours()
+    {
+        $this->makeTest('hour', false, 'next hour|next %hours% hours', array(1 => 1));
+
+    }
+
+    /**
+     * Testing normal next format minute.
+     */
+    public function testNextMinutes()
+    {
+        $this->makeTest('minute', false, 'next minute|next %minutes% minutes', array(1 => 1, 12 => 12, 10 => 10, 15 => 15));
+        $this->makeSimpleTest("minute", false, 'next half hour', array(30, 34, 28));
+    }
+
+    /**
+     * Testing normal next format seconds.
+     */
+    public function testNextSeconds()
+    {
+        $this->makeTest('second', false, 'next second|next %seconds% seconds', array(1 => 1, 12 => 12, 10 => 10, 15 => 15));
     }
 
     /**
      * Create test.
      *
-     * @param string $date
+     * @param $type
+     * @param $back
      * @param string $expect
-     * @param bool   $simple
+     * @param $params
+     * @param bool $simple
+     * @internal param string $date
      */
-    private function makeTest($date, $expect, $simple = false)
+    private function makeTest($type, $back, $expect, $params, $simple = false)
     {
         $now = new \DateTime('now');
-        $second = new \DateTime($date);
 
-        $result = $this->helper->formatTime($second, $now, $simple ? "simple" : null);
+        foreach ($this->languages as $lang => $data) {
+            foreach ($params as $given => $should) {
+                $second = clone $now;
+                $second->modify(($back ? "-" : "+") . $given . " " . $type);
 
-        $this->assertEquals($result, $expect);
+                $result = $data['helper']->formatTime($second, $now, $simple ? "simple" : null);
+
+                $p = array('%' . $type . 's%' => $should);
+
+                $expected = $data['translator']->transChoice($expect, $should, $p);
+
+                $this->assertEquals($expected, $result);
+            }
+        }
     }
 
+    private function makeSimpleTest($type, $back, $expect, $params)
+    {
+        $now = new \DateTime('now');
+
+        foreach ($this->languages as $lang => $data) {
+            foreach ($params as $given) {
+                $second = clone $now;
+                $second->modify(($back ? "-" : "+") . $given . " " . $type);
+
+                $result = $data['helper']->formatTime($second, $now, "simple");
+
+                $expected = $data['translator']->trans($expect);
+
+                $this->assertEquals($expected, $result);
+            }
+        }
+    }
 }
